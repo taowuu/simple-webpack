cbc76e6930e74a69df1962bf46ae5226074e6c4d
tao <2098613620@qq.com>
2022-09-25 16:56:41 +0800

    1

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
 
 var _greeting = require("./greeting.js");
 
-document.write((0, _greeting.greeting)('Jane')); },'./greeting.js': function (require, module, exports) { "use strict";
+document.write((0, _greeting.greeting)(' Tao')); },'./greeting.js': function (require, module, exports) { "use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
